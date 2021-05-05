4330cb37bd9faffa7900a880cb6b25282ecaab8a
kirti18patel <kirti18patel@yahoo.com>
2021-05-04 22:44:52 -0400

    possible inputs added

--- assets/javscript/javascript.js
+++ assets/javscript/javascript.js
@@ -1,8 +1,16 @@
+var setOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var setOfLowercaseAlphabets = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+var setOfUppercaseAlphabets = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+var setOfSpecialCharacters = [];
 var inputLength = function(){
     return (prompt("How many characters would you like your password to contain?"));
 };
 
-var passwordGenerator = function() {
+var passwordGenerator = function(){
+    alert("you are in pass");
+};
+
+var collectData = function() {
     var length = inputLength();
     if(!isNaN(length) && length!=null && length<=20 && length>=8){
         alert("You have entered password length : " + length);
@@ -10,12 +18,12 @@ var passwordGenerator = function() {
         var numbersInPassword = confirm("Click OK to add numeric characters.");
         var lowerCaseCharacterInPassword = confirm("Click OK to add lowercase character.");
         var upperCaseCharacterInPassword = confirm("Click OK to add uppercase characters.");
-
+        passwordGenerator();
     }
     else{
         var lengthConfirmation=confirm("You have entered wrong choice.\nClick OK to enter value again or click CANCEL to exit");
         if(lengthConfirmation){
-            passwordGenerator();
+            collectData();
         }
         else{
             alert("Thank You! Use Password Generator again!");
@@ -24,4 +32,4 @@ var passwordGenerator = function() {
 };
 
 var buttonEl = document.querySelector("#generate");
-buttonEl.addEventListener("click", passwordGenerator);
+buttonEl.addEventListener("click", collectData);
